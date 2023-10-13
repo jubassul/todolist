e1609a7e188fb75baff3fc2315c187a1ef32ffe2
jubassul <jubassul@gmail.com>
2023-10-13 11:49:38 -0300

    create task and validate user

--- todo-list/src/main/java/br/com/julia/todo/user/UserController.java
+++ todo-list/src/main/java/br/com/julia/todo/user/UserController.java
@@ -2,6 +2,9 @@ package br.com.julia.todo.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +27,10 @@ public class UserController {
       // status code
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
+    // criptografia de senha
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashred);
     var userCreated = userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
